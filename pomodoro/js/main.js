52c4e18849df3ab320ebfad6c67523d5def6b4d3
MzDevOcotal <mrzapata74@gmail.com>
2022-02-25 00:12:39 -0600

    Finalizado Pomodoro - Faltan Estilos

--- pomodoro/js/main.js
+++ pomodoro/js/main.js
@@ -4,9 +4,15 @@ let timer = null;
 let timerBreak = null;
 let current = null;
 
+
 const bAdd = document.querySelector('#bAdd');
 const itTask = document.querySelector('#itTask');
 const form = document.querySelector('#form');
+const taskName = document.querySelector('#time #taskname'); // Seleccionando el nombre de la tarea
+
+renderTime();
+renderTask();
+
 
 //Creando evento al presionar el botón submit
 form.addEventListener('submit', e => {
@@ -16,6 +22,8 @@ form.addEventListener('submit', e => {
         itTask.value = '';
         renderTask();
     }
+/*     console.log(tasks); */
+
 });
 
 //Fución para Crear una nueva tarea.
@@ -34,7 +42,7 @@ function renderTask(){
     const html = tasks.map(task =>{
         return `
             <div class="task">
-                <div class="completed">${task.completed ? `<span class='done'>Listo</span>`: `<button class="star-button" data-id="${task.id}">Iniciar</button>`}</div>
+                <div class="completed">${task.completed ? `<span class='done'>Listo</span>`:`<button class="start-button" data-id="${task.id}">Iniciar</button>`}</div>
                 <div class="title">${task.title}</div>
             </div>
         `
@@ -42,4 +50,82 @@ function renderTask(){
 
     const taskContainer = document.querySelector("#tasks");
     taskContainer.innerHTML = html.join('')//map regresa un arreglo de sring, se usa join para transformar ese arreglo en un solo string unidos con un espacio vacío
+
+    const startButtons = document.querySelectorAll('.task .start-button'); //Seleccionando todos los botones con la clase ".start-button"
+
+
+
+
+
+    startButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if(!timer){
+                const id = button.getAttribute('data-id');
+                startButtonHandler(id);
+                button.textContent = 'En Progreso...'
+            }
+        })
+    })
+
+}
+
+function startButtonHandler(id){
+    time = 5; //Conversión a segundos
+    current = id; //Almacenando el id actual
+    const taskIndex = tasks.findIndex((task) => task.id === id); //Encontraando la trea actual a través del index
+    taskName.textContent = tasks[taskIndex].title;//Sustituyendo el nombre por la tarea actual
+
+    /* DANDOLE FORMATO AL TIEMPO */
+    timer = setInterval(() => {
+        timeHandler(id);
+    }, 1000);
+}
+
+function timeHandler(id){
+    time--;
+    renderTime();
+
+    //Detener el tiempo cuando llegue a cero.
+    if(time === 0){
+        clearInterval(timer);
+        markCompleted(id);
+        timer = null;
+        renderTask();
+        startBreak();
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector('#time #value');
+    const minutos = parseInt(time/60);
+    const segundos = parseInt(time%60);
+
+    timeDiv.textContent = `${minutos < 10 ? '0' : ''}${minutos}:${segundos < 10 ? '0' : ''}${segundos}`
+}
+
+function markCompleted(id){
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    tasks[taskIndex].completed = true;
+}
+
+function startBreak(){
+    time = 3;
+    taskName.textContent = 'Break';
+    timerBreak = setInterval(() => {
+        timerBreakHandler();
+    }, 1000);
+}
+
+function timerBreakHandler(){
+    time--;
+    renderTime();
+
+    //Detener el tiempo cuando llegue a cero.
+    if(time === 0){
+        clearInterval(timerBreak);
+        current = null;
+        timerBreak = null;
+        taskName.textContent = '';
+        renderTask();
+    }
 }
